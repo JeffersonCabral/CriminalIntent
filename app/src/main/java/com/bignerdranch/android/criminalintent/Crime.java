0228ec64eeef122a3a202632d16632b4d8fdbb2d
Jefferson Cabral <jcabral1500@gmail.com>
2017-06-06 17:42:29 -0300

    Inclusão dos getters and setters da classe crime

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,8 +1,49 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by LENOVO on 05/06/2017.
  */
 
 public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 }
