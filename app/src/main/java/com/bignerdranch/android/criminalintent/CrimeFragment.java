16cf080e40d44b0bed290c89dbe5d657d35e87f3
Jefferson Cabral <jcabral1500@gmail.com>
2017-06-06 23:47:08 -0300

    criação da classe FragmentCrime e do layout crime_fragment.xml

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by LENOVO on 06/06/2017.
+ */
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_crime, parent, false);
+        return view;
+    }
+}
